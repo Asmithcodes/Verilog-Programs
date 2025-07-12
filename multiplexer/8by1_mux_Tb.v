4ae5ec49a779298cd0b590ee95d760c2b9df42b9
unknown <pampana.asmith@gmail.com>
2025-07-12 21:31:51 +0530

    Syntax update

--- multiplexer/8by1_mux_Tb.v
+++ multiplexer/8by1_mux_Tb.v
@@ -2,10 +2,11 @@ module 8by1_mux_Tb;
     reg [7:0] dataIn;
     reg [2:0] selectLine;
     wire dataOut;
+    integer i;
 
     8by1_mux uut(dataIn, selectLine, dataOut);
     initial begin
-        for(integer i = 0; i < 8; i = i + 1) begin
+        for(i = 0; i < 8; i = i + 1) begin
             selectLine = i;
             dataIn = 1 >> i;
             #20;
