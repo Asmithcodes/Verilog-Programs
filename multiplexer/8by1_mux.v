250ea990c8a408beaa48bad9b94aa67c477bdf61
unknown <pampana.asmith@gmail.com>
2025-07-12 21:25:52 +0530

    Multiplexer Behavioral model

---
+++ multiplexer/8by1_mux.v
@@ -0,0 +1,24 @@
+module 8by1_mux(
+    input [7:0] dataIn,
+    input [2:0] selectLine,
+    output reg dataOut
+    );
+
+    always @(*) begin
+    case(selectLine)
+        0: dataOut = dataIn[0]; 
+        1: dataOut = dataIn[1]; 
+        2: dataOut = dataIn[2]; 
+        3: dataOut = dataIn[3]; 
+        4: dataOut = dataIn[4]; 
+        5: dataOut = dataIn[5]; 
+        6: dataOut = dataIn[6]; 
+        7: dataOut = dataIn[7]; 
+    endcase
+    end
+
+    initial begin
+        dataIn = 0;
+        selectLine = 0;
+    end
+endmodule
---
+++ multiplexer/8by1_mux_Tb.v
@@ -0,0 +1,15 @@
+module 8by1_mux_Tb;
+    reg [7:0] dataIn;
+    reg [2:0] selectLine;
+    wire dataOut;
+
+    8by1_mux uut(dataIn, selectLine, dataOut);
+    initial begin
+        for(integer i = 0; i < 8; i = i + 1) begin
+            selectLine = i;
+            dataIn = 1 >> i;
+            #20;
+        end
+        $finish;
+    end
+endmodule
