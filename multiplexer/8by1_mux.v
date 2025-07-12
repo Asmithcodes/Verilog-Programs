02305ee1a585851845d069fc8edad53ac505ad79
unknown <pampana.asmith@gmail.com>
2025-07-12 21:27:23 +0530

    Indent update

--- multiplexer/8by1_mux.v
+++ multiplexer/8by1_mux.v
@@ -5,16 +5,16 @@ module 8by1_mux(
     );
 
     always @(*) begin
-    case(selectLine)
-        0: dataOut = dataIn[0]; 
-        1: dataOut = dataIn[1]; 
-        2: dataOut = dataIn[2]; 
-        3: dataOut = dataIn[3]; 
-        4: dataOut = dataIn[4]; 
-        5: dataOut = dataIn[5]; 
-        6: dataOut = dataIn[6]; 
-        7: dataOut = dataIn[7]; 
-    endcase
+        case(selectLine)
+            0: dataOut = dataIn[0]; 
+            1: dataOut = dataIn[1]; 
+            2: dataOut = dataIn[2]; 
+            3: dataOut = dataIn[3]; 
+            4: dataOut = dataIn[4]; 
+            5: dataOut = dataIn[5]; 
+            6: dataOut = dataIn[6]; 
+            7: dataOut = dataIn[7]; 
+        endcase
     end
 
     initial begin
