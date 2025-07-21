1a612bbb8f2fa7c1b65f8baa0c8cc8930203c384
Asmith Pampana <129410559+Asmithcodes@users.noreply.github.com>
2025-07-21 23:22:20 +0530

    Edited the variables

--- All_Gates/Logic_Gates.v
+++ All_Gates/Logic_Gates.v
@@ -1,14 +1,14 @@
 module All_Logic_Gates(
     input a, b,
-    output [7:0] out
+    output yNota, yBuf, yAnd, yOr, yNand, yNor, yXor, yXnor
     );
 
-    not(out[0], a);
-    buf(out[1], b);
-    and(out[2], a, b);
-    or(out[3], a, b);
-    nand(out[4], a, b);
-    nor(out[5], a, b);
-    xor(out[6], a, b);
-    xnor(out[7], a, b);
+    not(yNota, a);
+    buf(yBuf, b);
+    and(yAnd, a, b);
+    or(yOr, a, b);
+    nand(yNand, a, b);
+    nor(yNor, a, b);
+    xor(yXor, a, b);
+    xnor(yXnor, a, b);
 endmodule
