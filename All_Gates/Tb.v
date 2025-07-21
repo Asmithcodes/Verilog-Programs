a06dd633f65b7daedf3e3b6e9dae7f5d3ed9e4de
Asmith Pampana <129410559+Asmithcodes@users.noreply.github.com>
2025-07-21 23:27:43 +0530

    Added new Test bench file

---
+++ All_Gates/Tb.v
@@ -0,0 +1,13 @@
+module Tb;
+    reg a, b;
+    wire yNota, yBuf, yAnd, yOr, yNand, yNor, yXor, yXnor;
+
+    Logic_Gates dut(a, b, yNota, yBuf, yAnd, yOr, yNand, yNor, yXor, yXnor);
+    initial begin
+        {a, b} = 0; #20
+        {a, b} = 1; #20
+        {a, b} = 2; #20
+        {a, b} = 3; #20
+        $finish;
+    end
+endmodule
